91f9a2ca0c1e6a68d908ee66dce0bff33d80ad63
Azureki <fazureki@gmail.com>
2019-09-16 21:01:44 +0800

    cpp imp

---
+++ 58-翻转字符串/2-左旋转字符串/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void reverseString(string s, int left, int right) {
+        while (left < right) {
+            swap(s[left++], s[--right]);
+        }
+    }
+    string LeftRotateString(string str, int n) {
+        if (n < 0 || n > str.size()){
+            return str;
+        }
+        string res = str;
+        reverseString(res, 0, n);
+        reverseString(res, n, str.size());
+        reverseString(res, 0, str.size());
+        return res;
+    }
+};
+
