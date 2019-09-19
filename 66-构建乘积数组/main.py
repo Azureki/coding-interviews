7747b828488ace10861d61613ff22a139345d06e
Azureki <fazureki@gmail.com>
2019-09-19 15:19:38 +0800

    less space?

--- 66-构建乘积数组/main.py
+++ 66-构建乘积数组/main.py
@@ -10,6 +10,22 @@ class Solution:
         return [lst_c[i] * lst_d[i] for i in range(length)]
 
 # Actually, only one array(lst_c or lst_d) is needed.
+# However, in nowcoder, space occupation are both 5m
+
+"""
+class Solution:
+    def multiply(self, A):
+        length = len(A)
+        lst_c = [1] * length
+        # lst_d = [1] * length
+        for i in range(1, length):
+            lst_c[i] = lst_c[i - 1] * A[i - 1]
+        tem = 1
+        for i in range(length - 1, 0, - 1):
+            tem *= A[i]
+            lst_c[i-1] *= tem
+        return lst_c
+"""
 
 lst = [1,2,3,4,5]
 res = Solution().multiply(lst)
